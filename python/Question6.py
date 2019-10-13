fd2c5cc3657883322d2888a6db91b2b4dc87ff60
CJPJ007 <45278375+CJPJ007@users.noreply.github.com>
2019-10-12 21:09:30 -0700

    Update Question6.py

--- python/Question6.py
+++ python/Question6.py
@@ -1,5 +1,11 @@
 # modify this function, and create other functions below as you wish
+def mymethod(ch1,ch2,s):
+    idx=-1
+    for si in s:
+        if si[0]==ch1 and si[-1]==ch2 and idx<s.index(si):
+            idx=s.index(si)
+    return (idx)
 def question06(portfolio):
     # modify and then return the variable below
-    answer = -5
+    answer = mymethod('A','W',portfolio)
     return answer
