1ed5fd9aa23f3c62c7b2321f4dbcd00f06d228d3
CJPJ007 <45278375+CJPJ007@users.noreply.github.com>
2019-10-12 21:03:51 -0700

    Update Question2.py

--- python/Question2.py
+++ python/Question2.py
@@ -2,4 +2,7 @@
 def question02(risk, bonus, trader):
     # modify and then return the variable below
     answer = -5
+    for i in range(0,len(trader)):
+        lis.append(bonus[i]+trader[i]-risk[i])
+    answer = len(trader)*(bonus[lis.index(max(lis))])
     return answer
