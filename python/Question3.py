8b4dc2257b01f1d5abc11441eb36cf381a12bf8b
CJPJ007 <45278375+CJPJ007@users.noreply.github.com>
2019-10-12 21:29:05 -0700

    Update Question3.py

--- python/Question3.py
+++ python/Question3.py
@@ -2,4 +2,29 @@
 def question03(scores, alice):
     # modify and then return the variable below
     answer = -5
+    d={}
+    rank=[]
+    for i in alice:
+        scores.append(i)
+        scores.sort()
+        scores.reverse()
+        count=1
+        num=scores[0]
+        for j in range(len(scores)):
+            if(scores[j]<num):
+                count+=1
+                num=scores[j]
+                if(scores[j]==i):
+                    rank.append(count)
+                    break
+    for i in rank:
+        d[i]=0
+    for i in rank:
+        d[i]+=1
+    max=0
+    for i in sorted(d.keys()):
+        if(d[i]>max):
+            max=d[i]
+            idx=i
+    answer = idx
     return answer
